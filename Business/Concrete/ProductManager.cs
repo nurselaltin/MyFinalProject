3251b53f5d9b4125f723445a83a06db40539c4c0
Nursel ALTIN <nurselaltin.na@gmail.com>
2021-05-29 18:10:53 +0300

    Added Validation of CrossCuttingConcern to Core Layer.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,12 +1,16 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstarct;
 using DataAccess.Concrete;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
+
 using System.Text;
 
 namespace Business.Concrete
@@ -22,15 +26,13 @@ namespace Business.Concrete
         }
 
        
-
+        [Validate]
         public IResult Add(Product product)
         {
-            if(product.ProductName.Length < 2)
-            {
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
 
 
+
+            ValidationTool.Validate(new ProductValidator(), product);
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
         }
